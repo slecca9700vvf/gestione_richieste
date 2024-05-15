d445f30f219d08589c7abbbb46aecaf98f467631
simone.lecca <simone.lecca@vigilfuoco.it>
2024-05-15 13:07:07 +0200

    Create basic components

---
+++ src/Components/UI/Authentication/Authentication.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Authentication.css';
+
+function Authentication() {
+  return (
+    <div className="Authentication">{/*  */}
+      <header className="App-header">
+        <p>
+          Edit <code>Athentication</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default Authentication;
--- src/Components/UI/HomePage/HomePage.tsx
+++ src/Components/UI/HomePage/HomePage.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './HomePage.css';
 
-function App() {
+function HomePage() {
   return (
-    <div className="App">
+    <div className="Homepage">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit <code>HomePage</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -23,4 +22,4 @@ function App() {
   );
 }
 
-export default App;
+export default HomePage;
