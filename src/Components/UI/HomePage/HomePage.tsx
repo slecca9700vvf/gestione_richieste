5ebe36f341f3b918971775368db2f2841fb1da97
simone.lecca <simone.lecca@vigilfuoco.it>
2024-06-07 17:31:10 +0200

    Fixes

--- src/Components/UI/HomePage/HomePage.tsx
+++ src/Components/UI/HomePage/HomePage.tsx
@@ -1,28 +1,17 @@
 import React, { useState } from 'react';
-import CheckAuth from '../../Authentication/CheckAuth';
+import { CheckAuth, RetrieveUserData } from '../../Authentication/RetrieveAuthUser';
 import labels from '../../../API-Labels/labels.json'
 import { IUser } from '../../../Interfaces/IUser'
 
 function HomePage() {
 
   const isLogged = CheckAuth();
-
-  // TODO: Recuperare user dallo storage (?)
-  const [user, setUser] = useState<IUser>();
-  // const user:IUser = {
-  //   name: "pippo",
-  //   surname: "pluto",
-  //   idUtente: "paperino" 
-  // }
-
-  // setUser() = {
-
-  // }
+  const user = RetrieveUserData();
 
   return (
     <div className="homepage">
       <h2>{ labels.home_page.title }</h2>
-      <h3>{ isLogged ? 'Sei Loggato come: ' + user?.nome : 'Non sei loggato' }</h3>
+      <h3>{ isLogged ? 'Sei Loggato come: ' + user?.name + " " + user.surname: 'Non sei loggato' }</h3>
     </div>
   );
 }
