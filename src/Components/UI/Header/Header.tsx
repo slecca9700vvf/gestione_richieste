a3c749d14383203fe6be3b440bd42f8e7f60d604
antoniorobertoditerlizzi <133643377+antoniorobertoditerlizzi@users.noreply.github.com>
2024-06-10 16:26:36 +0200

    Update Header.tsx
    menu logout header

--- src/Components/UI/Header/Header.tsx
+++ src/Components/UI/Header/Header.tsx
@@ -1,6 +1,19 @@
 import React, { useState } from 'react';
 import { CheckAuth, RetrieveUserData } from '../../Authentication/RetrieveAuthUser';
 import labels from '../../../API-Labels/labels.json'
+import Avatar from '@mui/material/Avatar';
+import Stack from '@mui/material/Stack';
+import { deepOrange, deepPurple } from '@mui/material/colors';
+
+
+import ListSubheader from '@mui/material/ListSubheader';
+import List from '@mui/material/List';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+
+import Button from '@mui/material/Button';
+import Collapse from '@mui/material/Collapse';
 
 
 function Header() {
@@ -8,11 +21,14 @@ function Header() {
     const name = localStorage.getItem("user_name");
     const surname = localStorage.getItem("user_surname");
 
+	const [open, setOpen] = useState(false); 
+
+	const handleClick = () => {
+	setOpen(!open);
+	};
+		  
     return (
         <header className='header'>
-            <nav className='header-top'>
-               { labels.header.title } 
-            </nav>
             {/* TODO: Gestire voce di menu active 
                 <a className='nav-link active' ...>
             */}
@@ -25,19 +41,47 @@ function Header() {
                         <ul className='navbar-nav'>
                             <li className='nav-item'>
                                 <a className='nav-link' aria-current='page' href='/'>
-                                    <img src='/homepage.svg' title={labels.home_page.menu} alt={labels.home_page.menu}/> 
+                                    <img src='https://seeklogo.com/images/V/vigili-del-fuoco-logo-6CE6125E8E-seeklogo.com.png' width="20%" height="20%" title={labels.home_page.menu} alt={labels.home_page.menu}/> 
                                 </a>
                             </li>
-                            <li className='nav-item'>
-                                <a className='nav-link' href='/richiesta'>{  labels.forms.request.menu }</a>
-                            </li>
-                            <li className='nav-item'>
-                                <a className='nav-link' href='/settore'>{  labels.forms.sector.menu }</a>
-                            </li>
+                    
+                          
                         </ul>
-                        <span className='nav-item'>
-                        <a className='nav-link' href='/login'>{ isLogged ? name + " " + surname : 'Login' }</a>
+						
+						{/*<span className='nav-item'>
+							<a className='nav-link' href='/login'>{ isLogged ? name + " " + surname : 'Login' }</a>
                         </span>
+						
+                        <span className='nav-item'>
+							<a className='nav-link' href='/login'>{ isLogged ? <Avatar sx={{ bgcolor: deepPurple[500] }}>OP</Avatar> : 'Login' }</a>
+                        </span>*/}
+						
+						{/* inserire menu di 3 voci contenente Dettaglio utente - Info - Logout visibile al click sull'Avatar*/}
+						
+						 <div>
+						  <Button style={{backgroundColor: 'transparent', color: '#000', padding: '10px 20px'}} variant="contained"  onClick={handleClick} startIcon={<Avatar sx={{ bgcolor: deepPurple[500] }}>OP</Avatar>}>
+							{ isLogged ? name + " " + surname : 'Login' }
+						  </Button>
+						  <Collapse in={open} timeout="auto" unmountOnExit>
+							<List component="div" disablePadding>
+							  <ListItemButton href="/user-details">
+								  {/* Add icon for "Dettaglio Utente" */}
+								<ListItemText primary="Dettaglio Utente" />
+							  </ListItemButton>
+							  <ListItemButton href="/info">
+								  {/* Add icon for "Info" */}
+								<ListItemText primary="Info" />
+							  </ListItemButton>
+							  <ListItemButton href="/logout">
+								  {/* Add icon for "Logout" */}
+								<ListItemText primary="Logout" />
+							  </ListItemButton>
+							</List>
+						  </Collapse>
+						</div>
+							
+						  
+
                     </div>
                 </div>
             </nav>
