f1c9a0c47f30ca3bd5205d11756943eccfb605b0
antoniorobertoditerlizzi <133643377+antoniorobertoditerlizzi@users.noreply.github.com>
2024-06-28 16:39:10 +0200

    Implementazione n.4 Grafici in highcharts Dashboard
    Implementazione n.4 Grafici in highcharts Dashboard

--- src/Components/UI/HomePage/HomePage.tsx
+++ src/Components/UI/HomePage/HomePage.tsx
@@ -1,19 +1,33 @@
-import React, { useState } from 'react';
+import React, { useEffect } from 'react';
 import { CheckAuth, RetrieveUserData } from '../../Authentication/RetrieveAuthUser';
-import labels from '../../../API-Labels/labels.json'
-import { IUser } from '../../../Interfaces/IUser'
+import labels from '../../../API-Labels/labels.json';
+import { IUser } from '../../../Interfaces/IUser';
+import DashboardGraphs from '../../../Components/Graphs/Dashboard/DashboardGraphs';
 
 function HomePage() {
+    const isLogged = CheckAuth();
+    const user = RetrieveUserData();
 
-  const isLogged = CheckAuth();
-  const user = RetrieveUserData();
+    useEffect(() => {
+        // Forza il re-render del layout Flexbox
+        const appContent = document.querySelector('.app-content') as HTMLElement;
+        if (appContent) {
+            appContent.style.display = 'block';
+            setTimeout(() => {
+                appContent.style.display = 'flex';
+            }, 0);
+        }
+    }, []);
 
-  return (
-    <div className="homepage">
-      <h2>{ labels.home_page.title }</h2>
-      <h3>{ isLogged ? 'Sei Loggato come: ' + user?.name + " " + user.surname: 'Non sei loggato' }</h3>
-    </div>
-  );
+    return (
+        <div className="homepage">
+            <h2>{labels.home_page.title}</h2>
+            <h3>{isLogged ? 'Sei Loggato come: ' + user?.name + " " + user.surname : 'Non sei loggato'}</h3>
+            <div className="dashboard-container">
+                <DashboardGraphs />
+            </div>
+        </div>
+    );
 }
 
 export default HomePage;
