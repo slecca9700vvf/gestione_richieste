f7a9fcfb9ff038a817a69b20ef875323cbf1049c
simone.lecca <simone.lecca@vigilfuoco.it>
2024-07-01 12:17:44 +0200

    Setup tabs for homepage no-authenticated users

--- src/Components/UI/HomePage/HomePage.tsx
+++ src/Components/UI/HomePage/HomePage.tsx
@@ -1,24 +1,23 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { CheckAuth, RetrieveUserData } from '../../Authentication/RetrieveAuthUser';
 import Dashboard from '../Dashboard/Dashboard';
 import { getLabelByName } from "../../Exports/Labels";
+import TabNavigation from '../TabNavigation/TabNavigation';
+import tabs from '../../../API-Labels/defaultTabsHomePage.json'
 
 const HomePage = () => {
   const isLogged = CheckAuth();
-  const user = RetrieveUserData();
+  const classes = "homepage--wrapper" + " " + (isLogged ? "auth-true" : "auth-false");
 
-  //TODO Remove next line and setup components
-  const defultMessage = `In questa sezione sarà visibile l'elenco di: Novità software, documentazione, contatti utili!`
   return (
-    <div className="homepage">
+    <div className={ classes }>
       { isLogged ? (
         <Dashboard/>
       ) : (
-        <label>
-          Home Page
-          <br/>
-          { defultMessage }
-        </label>
+        <div>
+          <label className='homepage--auth-false--label'>{ getLabelByName("home_page_default_message") }</label>
+          <TabNavigation tabs_data={ tabs }></TabNavigation>
+        </div>
       )
       }
     </div>
---
+++ src/Components/UI/TabNavigation/TabNavigation.tsx
@@ -0,0 +1,45 @@
+import * as React from 'react';
+import { ITab } from '../../../Interfaces/ITab';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Box from '@mui/material/Box';
+import TabPanel from '@mui/lab/TabPanel';
+import TabContext from '@mui/lab/TabContext';
+
+interface ITabsProp {
+    tabs_data: Array<ITab>
+} 
+
+const TabNavigation = ({tabs_data}:ITabsProp) => {
+  const [value, setValue] = React.useState("1");
+  const handleChange = (event: React.SyntheticEvent, newValue: string) => {
+    setValue(newValue);
+  };
+
+  return (
+    <TabContext value={value}>
+      <Box sx={{ width: '100%' }} className="tab-navigation--box">
+        <Tabs 
+            value={value}
+            onChange={handleChange}
+            variant="fullWidth"
+            centered
+            className='tab-navigation--tabs'
+        >
+            {
+              tabs_data?.map((tab:ITab, index) => (
+                  <Tab className={'tab-navigation--tabs--tab' + ' ' + 'bottom_color_'+tab.color} key={index} label={ tab.label } value={ tab.value }/>
+              ))
+            }
+        </Tabs>
+        {
+          tabs_data?.map((tab:ITab, index) => (
+              <TabPanel className={'tab-navigation--tab-panel'} key={index} value={ tab.value }> { tab.content } </TabPanel>
+          ))
+        }
+      </Box>
+    </TabContext>
+  )
+}
+
+export default TabNavigation;
